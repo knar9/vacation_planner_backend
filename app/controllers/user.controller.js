0e0fed9a22834b86272731f93a52dae5369d1dee
Okan <oarslan2@smail.uni-koeln.de>
2022-12-23 14:05:09 +0100

    api for AllVacationRequestsScreen update response of API request

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -3,13 +3,13 @@ exports.allAccess = (req, res) => {
 };
 
 exports.userBoard = (req, res) => {
-  res.status(200).send("User Content.");
+  res.status(200).send(true);
 };
 
 exports.adminBoard = (req, res) => {
-  res.status(200).send("Admin Content.");
+  res.status(200).send(true);
 };
 
 exports.moderatorBoard = (req, res) => {
-  res.status(200).send("Moderator Content.");
+  res.status(200).send(true);
 };
